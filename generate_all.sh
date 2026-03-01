163e97c951c79299ead4e60865ab10c13aa738e4
Georg Graf (SPG) <grafg@spengergasse.at>
2026-03-01 06:07:25 +0100

    add alien theme, fix dark-mode blend for dual-image themes #11

--- generate_all.sh
+++ generate_all.sh
@@ -17,7 +17,15 @@ python extract_bootstrap_colors.py -i "$INPUT_BOOTSTRAP" -v "$VARS_FILE" -o "$OV
 
 echo "2. Generating Themes..."
 
-# Krokus (with Dark Image)
+# Lego
+echo "   - Lego"
+python ColorSim.py img/bg-lego-lightblue.png --dark-image img/bg-lego-darkblue.png --output bs/lego-theme.css --clusters 12
+
+# Aliens
+echo "   - Aliens"
+python ColorSim.py img/bg-alien-day.jpg --dark-image img/bg-alien-night.jpg --output bs/alien-theme.css --clusters 32 --dark-clusters 32
+
+# Krokus
 echo "   - Krokus"
 python ColorSim.py img/bg-krokus.jpg --output bs/krokus-theme.css --clusters 12
 
@@ -28,10 +36,6 @@ python ColorSim.py img/herbst.jpg --output bs/herbst-theme.css --clusters 12
 # Sommer
 echo "   - Sommer"
 python ColorSim.py img/sommer.jpg --output bs/sommer-theme.css --clusters 12
-
-# Lego
-echo "   - Lego"
-python ColorSim.py img/bg-lego-lightblue.png --dark-image img/bg-lego-darkblue.png --output bs/lego-theme.css --clusters 12
 
 # Loewe
 echo "   - Loewe"
